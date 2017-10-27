84908e38d7d38027f0fbe6f98f453100d85391e3
PovilasKorop <povilask007@gmail.com>
2017-10-27 12:59:59 +0300

    Update UsersController.php

--- app/Http/Controllers/Admin/UsersController.php
+++ app/Http/Controllers/Admin/UsersController.php
@@ -23,7 +23,7 @@ class UsersController extends Controller
             return abort(401);
         }
 
-        $users = User::all()->load('roles');
+        $users = User::with('roles')->get();
 
         return view('admin.users.index', compact('users'));
     }
